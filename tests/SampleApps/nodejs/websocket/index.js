fa102fc255aecdb5b62ad71d83eafa5fc6f3ec3f
Dor Fire <dorfire@microsoft.com>
2019-01-17 16:19:04 -0800

    Modified sample app to have a default port

--- tests/SampleApps/nodejs/websocket/index.js
+++ tests/SampleApps/nodejs/websocket/index.js
@@ -5,12 +5,10 @@ var server = require('http').createServer(app);
 
 var io = require('socket.io')(server);
 
-var port = process.env.PORT;
-
+var port = process.env.PORT || 8080;
 
 
 server.listen(port, function () {
-
   console.log('Server listening at port %d', port);
 });
 

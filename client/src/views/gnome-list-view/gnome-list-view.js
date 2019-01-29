2380a6d05d91c10737c628eba8be9f280ee93cb7
Bowden Kelly <wilkelly@microsoft.com>
2019-01-29 10:25:47 -0800

    added loading spinner

--- client/src/views/gnome-list-view/gnome-list-view.js
+++ client/src/views/gnome-list-view/gnome-list-view.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import GnomeView from '../gnome-view/gnome-view';
+import LoadingSpinner from '../loading-spinner/loading-spinner';
 import './gnome-list-view.css';
 
 export default React.createClass({
@@ -13,9 +14,10 @@ export default React.createClass({
 
     render() {
         const items = this.props.items.map((item, key) => <GnomeView key={key} item={item} createExpandItemAction={this.props.createExpandItemAction} />);
+        const itemsView = this.props.items.length === 0 ? <LoadingSpinner /> : items
         return (
             <div className="gs-gnomelist">
-                {items}
+                {itemsView}
             </div>
         );
     }
---
+++ client/src/views/loading-spinner/loading-spinner.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Icon from 'react-fa';
+import { createAddToCartAction } from '../../actions/cart-actions';
+
+import './loading-spinner.css';
+
+export default React.createClass({
+
+    displayName: 'loading-spinner',
+
+    render() {
+        return (
+            <div>
+                <div className="loading-spinner">
+                    <p className="text-center">
+                        <span className="fa fa-spinner fa-spin fa-3x"></span>
+                    </p>
+                </div>
+                {/* <p>Awaiting connection...</p> */}
+            </div>
+        );
+    }
+});

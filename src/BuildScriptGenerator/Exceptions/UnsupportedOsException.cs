59808e63b7129eb4475dd7bc343ed6dc07481635
Dor Fire <dorfire@microsoft.com>
2019-07-05 13:29:16 -0700

    Added exception

---
+++ src/BuildScriptGenerator/Exceptions/UnsupportedOsException.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+namespace Microsoft.Oryx.BuildScriptGenerator.Exceptions
+{
+    /// <summary>
+    /// Current operating system is unsupported.
+    /// </summary>
+    public class UnsupportedOsException : InvalidUsageException
+    {
+        public UnsupportedOsException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ src/BuildScriptGenerator/Helpers/OsHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Microsoft.Oryx.BuildScriptGenerator.Helpers
+{
+    internal static class OsHelper
+    {
+        private const string DebianVersionFilePath = "/etc/debian_version";
+        private const string DebianStretchMajorVersion = "9";
+
+        /// <summary>
+        /// Determines if the current OS is Debian Stretch.
+        /// </summary>
+        /// <returns>true if running on Stretch, false otherwise</returns>
+        public static bool IsDebianStretch()
+        {
+            if (!File.Exists(DebianVersionFilePath))
+            {
+                return false;
+            }
+
+            try
+            {
+                var debianVersion = File.ReadAllText(DebianVersionFilePath);
+                return debianVersion.Trim().StartsWith(DebianStretchMajorVersion);
+            }
+            catch
+            {
+            }
+
+            return false;
+        }
+    }
+}

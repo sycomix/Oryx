7ee8254d41b915151bf567bdb54de30a16538cba
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2020-01-10 11:47:31 -0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -102,35 +102,45 @@ echo Handling TailwindTrader app deployment.
 
 # 1. Install npm packages
 if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
-  cd "$DEPLOYMENT_SOURCE"
-  rm -rf node_modules
-  rm package-lock.json
-  ls -l
-  echo "Running npm cache clean"
-  eval npm cache clean --force
-  exitWithMessageOnError "npm cache clean failed"
-  echo "Running npm install with microsoft standard cdn endpoint started: "$SECONDS
-  start1=$SECONDS
-  eval npm install --registry https://arroyc-st-msft.azureedge.net 
-  exitWithMessageOnError "npm install from cdn endpoint failed"
-  end1=$SECONDS
-  echo "************************************************************"
-  echo "time taken for installation from cdn: "$(( $end1 - $start1 ))
-  echo "************************************************************"
-  rm -rf node_modules
-  rm package-lock.json
-  ls -l
-  echo "Running npm cache clean"
-  eval npm cache clean --force
-  exitWithMessageOnError "npm cache clean failed"
-  echo "Running npm install with standard npm registry started: "$SECONDS
-  start2=$SECONDS
-  eval npm install 
-  exitWithMessageOnError "npm install from npm standard registry failed"
-  end2=$SECONDS
-  echo "************************************************************"
-  echo "time taken for installation from npm: "$(( $end2 - $start2 ))
-  echo "************************************************************"
+  printf "BuildId\tWebAppName\tDate\tBuildStartTick\tBuildEndTick\tDurationInSeconds\tWithCDN\n" >> /home/site/wwwroot/log.csv
+  for i in {1..5}
+  do 
+    cd "$DEPLOYMENT_SOURCE"
+    rm -rf node_modules
+    rm package-lock.json
+    ls -l
+    echo "Running npm cache clean"
+    eval npm cache clean --force
+    eval npm cache verify
+    exitWithMessageOnError "npm cache clean failed"
+    echo "Running npm install with microsoft standard cdn endpoint started: "$SECONDS
+    start1=$SECONDS
+    eval npm install --registry https://arroyc-st-msft.azureedge.net 
+    exitWithMessageOnError "npm install from cdn endpoint failed"
+    end1=$SECONDS
+    duration1=$(( $end1 - $start1 ))
+    printf "$i-1\t$APPSETTING_WEBSITE_SITE_NAME\t$(date)\t$start1\t$end1\t$duration1\tYes\n" >> /home/site/wwwroot/log.csv
+    echo "************************************************************"
+    echo "time taken for installation from cdn: "$(( $end1 - $start1 ))
+    echo "************************************************************"
+    rm -rf node_modules
+    rm package-lock.json
+    ls -l
+    echo "Running npm cache clean"
+    eval npm cache clean --force
+    exitWithMessageOnError "npm cache clean failed"
+    echo "Running npm install with standard npm registry started: "$SECONDS
+    start2=$SECONDS
+    eval npm install  --registry https://registry.npmjs.org
+    exitWithMessageOnError "npm install from npm standard registry failed"
+    end2=$SECONDS
+    duration1=$(( $end2 - $start2 ))
+    printf "$i-2\t$APPSETTING_WEBSITE_SITE_NAME\t$(date)\t$start1\t$end1\t$duration1\tYes\n" >> /home/site/wwwroot/log.csv
+    echo "************************************************************"
+    echo "time taken for installation from npm: "$(( $end2 - $start2 ))
+    echo "************************************************************"
+  done
+  
  cd - > /dev/null
 fi
 
